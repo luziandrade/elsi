1372f42b0b30b6bfc6c110f7b933546467f1ebc1
luziandrade <luzi015@hotmail.com>
2020-05-13 18:26:16 +0100

    heroku settings

--- elsi/settings.py
+++ elsi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['iels.heroku.com']
+ALLOWED_HOSTS = ['iels.herokuapp.com']
 
 
 # Application definition
